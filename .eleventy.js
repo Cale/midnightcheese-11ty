07ff512eef65617d060f4cb46c8bf1e960e5a0d0
Cale <cale@midnightcheese.com>
2021-03-11 18:38:14 -0600

    Add RSS and CSS.

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,20 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    excerpt_alias: 'post_excerpt'
+  });
+  eleventyConfig.addPassthroughCopy('css')
+  return {
+    passthroughFileCopy: true
+  }
+
   return {
     dir: {
       input: "./",
       output: "./_site"
     }
   };
-};
-
-module.exports = function(eleventyConfig) {
-  eleventyConfig.setFrontMatterParsingOptions({
-    excerpt: true,
-    excerpt_alias: 'post_excerpt'
-  });
 };
