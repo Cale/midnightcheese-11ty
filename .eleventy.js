fa71b597de9374697531f4e76308bf8b6e6b3867
Cale Mooth <cale@midnightcheese.com>
2021-03-17 22:01:55 -0500

    Render tags list and pages.

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,35 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
     excerpt_alias: 'post_excerpt'
   });
-  eleventyConfig.addPassthroughCopy('css')
-  return {
-    passthroughFileCopy: true
-  }
+
+  eleventyConfig.addFilter("filterTagList", tags => {
+    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+  });
+
+  eleventyConfig.addFilter("formatDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLLL d, yyyy");
+  });
+
+  eleventyConfig.addCollection("tagList", function(collection) {
+    let tagSet = new Set();
+    collection.getAll().forEach(item => {
+      (item.data.tags || []).forEach(tag => tagSet.add(tag));
+    });
+    return [...tagSet];
+  });
 
   return {
+    passthroughFileCopy: true,
     dir: {
       input: "./",
       output: "./_site"
     }
-  };
+  }
 };
